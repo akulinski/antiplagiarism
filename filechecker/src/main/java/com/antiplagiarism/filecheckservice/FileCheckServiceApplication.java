2f289e0c95c06749f67d538908dc1e26b95ad1cc
Albert <albert.kulinski@hotmail.com>
2019-03-08 13:42:22 +0100

    Created basic structure of file checker service

---
+++ filechecker/src/main/java/com/antiplagiarism/filecheckservice/FileCheckServiceApplication.java
@@ -0,0 +1,13 @@
+package com.antiplagiarism.filecheckservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCheckServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileCheckServiceApplication.class, args);
+    }
+
+}
