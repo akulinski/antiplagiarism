dd3faf7ab11279320fb8624b02ab42cc77074d6f
Albert <albert.kulinski@hotmail.com>
2019-03-14 18:36:31 +0100

    File checker service integration with elastic search

--- fileuploadservice/src/main/java/com/antiplagiarism/fileuploadservice/FileuploadserviceApplication.java
+++ fileuploadservice/src/main/java/com/antiplagiarism/fileuploadservice/FileuploadserviceApplication.java
@@ -2,21 +2,18 @@ package com.antiplagiarism.fileuploadservice;
 
 import com.antiplagiarism.fileuploadservice.config.ApplicationProperties;
 import com.antiplagiarism.fileuploadservice.config.FileStorageProperties;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.event.EventListener;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ApplicationProperties.class, FileStorageProperties.class})
 @EnableAsync
 @EntityScan(basePackages = {"com.antiplagiarism.fileuploadservice.domain.entites"})
+@EnableEurekaClient
 public class FileuploadserviceApplication {
 
     public static void main(String[] args) {
